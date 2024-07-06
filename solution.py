77066b70b9687701c3aa6df7ed68461488482338
Kooreis <kooreis.official@gmail.com>
2024-07-05 20:27:02 -0500

    The second update introduces another nested function 'place_queen'. This function recursively places queens on the board and returns all possible solutions.

--- solution.py
+++ solution.py
@@ -1,10 +1,13 @@
 ```python
 def solve_n_queens(n):
-    def can_place(pos, ocuppied_positions):
-        for i in range(len(ocuppied_positions)):
-            if ocuppied_positions[i] == pos or \
-                ocuppied_positions[i] - i == pos - len(ocuppied_positions) or \
-                ocuppied_positions[i] + i == pos + len(ocuppied_positions):
-                return False
-        return True
+    ...
+    def place_queen(n, ocuppied_positions=[]):
+        if n == len(ocuppied_positions):
+            return [ocuppied_positions]
+        else:
+            solutions = []
+            for pos in range(n):
+                if can_place(pos, ocuppied_positions):
+                    solutions += place_queen(n, ocuppied_positions + [pos])
+            return solutions
 ```
